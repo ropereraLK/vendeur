b74102cb4a53cc342667bc046279c3b3fe8cdb0b
CORPNET\roerlk <rohan.perera@ifs.com>
2024-10-09 23:47:09 +0530

    Created Spring boot project

---
+++ src/main/java/io/github/ropereralk/vendeur/vendeur/VendeurApplication.java
@@ -0,0 +1,13 @@
+package io.github.ropereralk.vendeur.vendeur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/ropereralk/vendeur/vendeur/VendeurApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ropereralk.vendeur.vendeur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
